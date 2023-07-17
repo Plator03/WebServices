b8041c44217258f8b0bada0a6990e10af84ec1c7
ToTiPC <muaxheritoti@gmail.com>
2023-07-17 19:00:53 +0200

    step15

--- src/main/java/com/example/rest/webservices/webservices/exception/CustomizedResponeEntityExceptionHandler.java
+++ src/main/java/com/example/rest/webservices/webservices/exception/CustomizedResponeEntityExceptionHandler.java
@@ -1,8 +1,12 @@
 package com.example.rest.webservices.webservices.exception;
 
 import com.example.rest.webservices.webservices.Users.UserNotFoundException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.lang.Nullable;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -14,21 +18,31 @@ import java.time.LocalDateTime;
 @ControllerAdvice
 public class CustomizedResponeEntityExceptionHandler extends ResponseEntityExceptionHandler {
 
-@ExceptionHandler(Exception.class)
-    public final ResponseEntity<ErrorDetails> handleAllException(Exception ex, WebRequest request) throws Exception{
+    @ExceptionHandler(Exception.class)
+    public final ResponseEntity<ErrorDetails> handleAllException(Exception ex, WebRequest request) throws Exception {
 
-    ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now(), ex.getMessage(),
-            request.getDescription(false));
+        ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now(), ex.getMessage(),
+                request.getDescription(false));
 
-    return new ResponseEntity<ErrorDetails>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+        return new ResponseEntity<ErrorDetails>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @ExceptionHandler(UserNotFoundException.class)
-    public final ResponseEntity<ErrorDetails> handleUserNotFoundException(Exception ex, WebRequest request) throws Exception{
+    public final ResponseEntity<ErrorDetails> handleUserNotFoundException(Exception ex, WebRequest request) throws Exception {
 
-    ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now(), ex.getMessage(),
-            request.getDescription(false));
+        ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now(), ex.getMessage(),
+                request.getDescription(false));
 
-    return new ResponseEntity<ErrorDetails>(errorDetails, HttpStatus.NOT_FOUND);
+        return new ResponseEntity<ErrorDetails>(errorDetails, HttpStatus.NOT_FOUND);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now(), ex.getMessage(),
+                request.getDescription(false));
+
+
+        return new ResponseEntity(errorDetails, HttpStatus.BAD_REQUEST);
+
     }
 }
